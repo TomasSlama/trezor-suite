2706c52db0e8cfec6b044e3ad031e34a761ad51b
TomasSlama <tomas.slama@toptal.com>
2024-11-02 13:11:36 +0100

    fixup! feat(suite#4941): add util function to determine final label

--- packages/product-components/src/components/PassphraseTypeCard/getSubmitLabel.tsx
+++ packages/product-components/src/components/PassphraseTypeCard/getSubmitLabel.tsx
@@ -11,18 +11,10 @@ export const getSubmitLabel = ({
 }) => {
     if (!nonAsciiChars) return label;
 
-    if (nonAsciiChars.length === 1) {
-        return (
-            <>
-                {label}{' '}
-                <FormattedMessage
-                    id="TR_NON_ASCII_CHAR"
-                    defaultMessage='(with non-recommended "{char}")'
-                    values={{ char: nonAsciiChars[0] }}
-                />
-            </>
-        );
-    }
+    const isSingleChar = nonAsciiChars.length === 1;
+    const messageId = isSingleChar ? 'TR_NON_ASCII_CHAR' : 'TR_NON_ASCII_CHARS';
+    const chars = isSingleChar ? '"{char}"' : 'characters';
+    const defaultMessage = `{label} (with non-recommended ${chars})`;
 
     return (
         <>
